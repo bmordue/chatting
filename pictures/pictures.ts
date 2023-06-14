d0158de482c4297fcacd7e50973d9980109dd34e
Ben Mordue <benmordue@gmail.com>
2023-06-14 19:11:59 +0100

    remove commented code

--- pictures/pictures.ts
+++ pictures/pictures.ts
@@ -11,15 +11,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function main() {
-  //   const messagesFilename = "messages.json";
-  //   const messagesArray: ChatCompletionResponseMessage[] = JSON.parse(
-  //     readFileSync(messagesFilename, "utf-8")
-  //   );
+
   const paramsFile = argv[2];
   const params = JSON.parse(readFileSync(paramsFile, "utf-8"));
   const response = await openai.createImage(params);
 
-  // console.log(JSON.stringify(response.data));
   const created = response.data.created;
   response.data.data.forEach((d: ImagesResponseDataInner, i) => {
     writeFileSync(`${created}-${i}.png`, Buffer.from(d.b64_json!, "base64"));
