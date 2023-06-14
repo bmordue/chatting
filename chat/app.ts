02ebca2785c095c3886e2e55a982b8275c7429f6
Ben Mordue <benmordue@gmail.com>
2023-06-14 19:07:49 +0100

    move stuff around

--- chat/app.ts
+++ chat/app.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import { readFileSync, readdir, writeFileSync } from "fs";
+import { join } from "path";
 config();
 
 import {
@@ -21,8 +22,7 @@ async function sanityCheck() {
 async function main() {
   // openai.listModels().then((resp) => resp.data).then(console.log);
 
-
-  const chatDir = "chat/";
+  const chatDir = "chats/";
 
   // list the files in chatDir
 
@@ -35,43 +35,32 @@ async function main() {
     files.forEach(async (file) => {
       // if the file does not end in .json, ignore it
       if (!file.endsWith(".json")) {
+        console.log(`ignoring ${file}: not a .json`);
         return;
       }
 
+      const filepath = join(chatDir, file);
+      const messagesArray: ChatCompletionResponseMessage[] = JSON.parse(readFileSync(filepath, "utf-8"));
+
+      if (messagesArray[messagesArray.length - 1].role !== 'user') {
+        console.log(`ignoring ${file}: no new prompt`)
+      }
+
+      console.log(`continuing the conversation in ${file}`);
       // read the file and parse it as JSON to get an array of ChatCompletionResponseMessage
-      const messagesArray: ChatCompletionResponseMessage[] = JSON.parse(readFileSync(file, "utf-8"));
-      const completion = await openai.createChatCompletion({ model: "gpt-3.5-turbo", messages: messagesArray });
-      const choices = completion.data.choices;
-      // write choices to a file suffixed with ".answers.json", so that "input1.json" becomes "input1.answers.json"
-      // remove the .json suffix from the file name
-      file = file.replace(".json", "");
-      messagesArray.push(completion.data.choices[0].message!);
-      writeFileSync(file, JSON.stringify(messagesArray));
-      writeFileSync(file.replace(".json", ".answers.json"), JSON.stringify(completion.data));
+      try {
+        const completion = await openai.createChatCompletion({ model: "gpt-3.5-turbo", messages: messagesArray });
+        const choices = completion.data.choices;
+
+        messagesArray.push(completion.data.choices[0].message!);
+        writeFileSync(join(chatDir, file), JSON.stringify(messagesArray));
+        writeFileSync(join(chatDir, file.replace(".json", ".answers.json")), JSON.stringify(completion.data));
+      } catch (e) {
+        console.error(`${file}: failed to complete chat`);
+      }
     });
 
   });
-  // read the file and parse it as JSON to get an array of ChatCompletionResponseMessage
-  // for each message with a User role
-  // create a chat completion with openai
-  // then write all answers to a JSON file with the same name as the input, but suffixed with ".answers.json", so that "input1.json" becomes "input1.answers.json"
-
-  //ignore any exceptions and continue with the next file
-
-  //  const messagesFilename = "chat/messages.json";
-  //  const messagesArray: ChatCompletionResponseMessage[] = JSON.parse(
-  //    readFileSync(messagesFilename, "utf-8")
-  //  );
-  //  const completion = await openai.createChatCompletion({
-  //    model: "gpt-3.5-turbo",
-  //    messages: messagesArray,
-  //  });
-  //  const reply = completion.data.choices[0].message;
-  //  console.log(reply);
-  //  if (reply) {
-  //    messagesArray.push(reply);
-  //    writeFileSync(messagesFilename, JSON.stringify(messagesArray));
-  //  }
 }
 
 main();
