5fa434eb0da04745086f7fc45f0b01af91cc811a
Ben Mordue <benmordue@gmail.com>
2023-06-19 21:47:15 +0100

    prompt variations

--- chats/parse.js
+++ chats/parse.js
@@ -13,8 +13,8 @@ function main() {
   const ideas = readFileSync('ideas.txt', 'utf-8').split('\n');
   ideas.forEach(title => {
     const templateJson = JSON.parse(readFileSync('messages.json', 'utf-8'));
-    templateJson[0].content = `what is ${title}?`; // `Write a short blog post about ${title}.`;
-    writeFileSync( `${title.slice(0,5).trim()}${Math.floor(Math.random()*10)}_what.chat.json`, JSON.stringify(templateJson));
+    templateJson[0].content = `is there such a thing as ${title}?`; // `what is ${title}?`; // `Write a short blog post about ${title}.`;
+    writeFileSync( `${title.slice(0,5).trim()}${Math.floor(Math.random()*10)}_such.chat.json`, JSON.stringify(templateJson));
   });
 
 
