6c2e8ee067a279ec6473f69cde99dd725f4ebf76
Ben Mordue <benmordue@gmail.com>
2023-06-19 18:58:48 +0100

    bunch of rubbish (wacky ideas) chats

--- chats/parse.js
+++ chats/parse.js
@@ -1,3 +1,24 @@
-const fs = require('fs');
+import {readFileSync, writeFileSync } from 'fs';
 
-fs.readFileSync('WackIdeas.md', 'utf-8');
+//const ideasFile = fs.readFileSync('WackyIdeas.md', 'utf-8');
+
+function main() {
+//  const title = process.argv[2];
+
+//  if (!title) {
+//console.log(`No title found in: ${process.argv.join(' ')}`);
+//process.exit(0);
+//}
+
+  const ideas = readFileSync('ideas.txt', 'utf-8').split('\n');
+  ideas.forEach(title => {
+    const templateJson = JSON.parse(readFileSync('messages.json', 'utf-8'));
+    templateJson[0].content = `Write a short blog post about ${title}.`;
+    writeFileSync( `${title.slice(0,5).trim()}${Math.floor(Math.random()*10)}.chat.json`, JSON.stringify(templateJson));
+  });
+
+
+
+}
+
+main();
