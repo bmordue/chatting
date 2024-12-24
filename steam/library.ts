c8d302eaecf460dd925012183b31c90cb2bf0f4e
Ben Mordue <benmordue@gmail.com>
2024-12-24 15:04:37 +0000

    steam wip

--- steam/library.ts
+++ steam/library.ts
@@ -18,20 +18,22 @@ if (!STEAM_API_KEY || !STEAM_USER_ID) {
 const getWishlistAndLibraryData = async () => {
   try {
     // Get user data
-    const userResponse = await fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${STEAM_USER_ID}`);
-    const userJson = await userResponse.json() as SteamUserResponse;
-    const userData = userJson.response.players[0];
+    // const userResponse = await fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${STEAM_USER_ID}`);
+    // const userJson = await userResponse.json() as SteamUserResponse;
 
-    if (!userData) {
-      console.error('Invalid Steam user ID');
-      return;
-    }
+    // console.log('Player summary response:\n\n' + JSON.stringify(userJson.response, null, 4));
+    // const userData = userJson.response.players[0];
+
+    // if (!userData) {
+    //   console.error('Invalid Steam user ID');
+    //   return;
+    // }
 
     // Get wishlist data
-    const wishlistResponse = await fetch(`https://store.steampowered.com/wishlist/profiles/${userData.steamid}/wishlistdata/`);
-    const wishlistData = await wishlistResponse.json() as SteamWishlistResponse;
+    const wishlistResponse = await fetch(`https://store.steampowered.com/wishlist/profiles/${STEAM_USER_ID}/wishlistdata/`);
+    const wishlistData = await wishlistResponse.json();
 
-    console.log(JSON.stringify(wishlistData, null, 4));
+    console.log('Wishlist:\n\n' + JSON.stringify(wishlistData, null, 4));
 
 
     // Get library data
@@ -42,7 +44,7 @@ const getWishlistAndLibraryData = async () => {
     const gamesInLibrary = [];
 
     // Check if each game in the wishlist is in the library
-    for (const appId in wishlistData) {
+    for (const appId in []) {//wishlistData) {
       const game = wishlistData[appId];
       const isInLibrary = libraryData.some((libraryGame: any) => libraryGame.appid === parseInt(appId));
 
@@ -60,7 +62,7 @@ const getWishlistAndLibraryData = async () => {
     const gamesWithDemos = [];
 
     // Check if a demo is available for each game in the wishlist
-    for (const appId in wishlistData) {
+    for (const appId in []) { //wishlistData) {
       const game = wishlistData[appId];
       // const hasDemo = game.demos && game.demos.length > 0;
 
